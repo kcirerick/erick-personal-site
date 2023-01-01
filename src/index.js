3bb48f1f9b4803101df1180d4d8afb82306abb06
kcirerick <enriquezerick182@gmail.com>
2023-01-01 14:07:33 -0700

    basic setup through step 6.

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  return <div>Welcome to Erick's World!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
